bfc301d87dab119ec5eb9f8b0e26945ae9c13085
Babatunde Olowoyeye <babatunde.olowoyeye@unitedcapitalplcgroup.com>
2024-01-17 00:13:54 +0100

    bcrypt hash

---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateUserDto } from './dto/user.dto';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async create(dto: CreateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (user) throw new ConflictException('User already exists');
+
+    const newUSer = await this.prisma.user.create({
+      data: {
+        ...dto,
+        password: await hash(dto.password, 10),
+      },
+    });
+
+    const { password, ...result } = newUSer;
+
+    return result;
+  }
+}
